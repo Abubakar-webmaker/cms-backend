981901a83d278b7153a345f96ab5c8e61197f2dd
Abubakar-webmaker <m.abubakar.codes@gmail.com>
2026-05-09 02:04:02 +0500

    fix: allow all origins CORS

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ app.disable("x-powered-by");
 
 // ─── CORS Manual ──────────────────────────────────────────
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://cms-frontend-three-kohl.vercel.app");
+  const origin = req.headers.origin;
+  res.header("Access-Control-Allow-Origin", origin || "https://cms-frontend-three-kohl.vercel.app/api");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,PATCH,OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
